40d5c2ceac9369e6148ba276419887b38abc285d
swwden <ranninharanna0@gmail.com>
2022-01-31 11:04:14 -0800

    commit1

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import chalk from 'chalk';
 import inquirer from 'inquirer';
 import figlet from 'figlet';
 import { createSpinner } from 'nanospinner';
+import LCUConnector from 'lcu-connector';
+const connector = new LCUConnector();
+
 
 const sleep = (ms = 2000) => new Promise((r) => setTimeout(r,ms));
 
@@ -53,5 +56,5 @@ async function ask2(){
 }
 
 async function handle2(input){
-    
+    console.log(input)
 }
